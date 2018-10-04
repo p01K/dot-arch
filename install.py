5a51c16c4af17b19746fcde362636b9221282328
p01K <pkatsogr@gmail.com>
2018-10-04 18:54:38 +0300

    script update

--- install.py
+++ install.py
@@ -14,20 +14,35 @@ def install(key):
     #shutil.copyfile(source_path,dest_path)
     return (source_path, dest_path)
 
+def install_all(keylist):
+    for k in keylist:
+        install(k)
+
+USER_HOME="/home/{}".format(user)
 
 filedict = {
-"alacritty":"home/",
-"tmux":("home/tmux.conf","/home/{}/.tmux.conf".format(user)),
-"zshrc":("home/zshrc","/home/{}/.zshrc".format(user)),
-"Xresources":("home/Xresources","/home/{}/.Xresources".format(user))
+"alacritty"  :("home/alacritty.yml","{}/.config/alacritty/alacritty.yml"),
+"tmux"       :("home/tmux.conf","{}/.tmux.conf"),
+"zshrc"      :("home/zshrc","{}/.zshrc"),
+"zlogin"     :("home/zlogin","{}/.zshrc"),
+"Xresources" :("home/Xresources","{}/.Xresources"),
+"micro"      :("home/micro.json","{}/.config/micro/settings.json"),
+"i3"         :("i3/i3config","{}/.i3/config"),
+"i3status"   :("i3/i3status.conf","{}/.i3status.conf")
 }
 
-if__name__== "__main__":
+for k in filedict.keys():
+    (src,dst)   = filedict[k]
+    filedict[k] = (src,dst.format(USER_HOME))
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--mod', type=str, required=True)
+print(filedict)
 
-    args = parser.parse_args()
+if __name__== "__main__":
+
+    parser   = argparse.ArgumentParser()
+    help_msg = "possible modules {}".format(filedict.keys())
+    parser.add_argument('-m', '--mod', type=str, required=True, help=help_msg)
+    args     = parser.parse_args()
 
     print(args.mod)
     print(filedict.contains_key(args.mod))
