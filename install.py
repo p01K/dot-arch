5b2e7f2ce510165d22a0732af5e935ba18d9690e
p01K <pkatsogr@gmail.com>
2018-11-30 19:30:27 +0200

    conflict

--- install.py
+++ install.py
@@ -5,13 +5,11 @@ import argparse
 
 user = os.environ['USER']
 
-print(user)
-
 def install(key):
     source_path, dest_path = filedict[key]
-    print(os.path.isfile(source_path))
-    print(os.path.isfile(dest_path))
-    #shutil.copyfile(source_path,dest_path)
+    assert(os.path.isfile(source_path))
+    #assert(os.path.isfile(dest_path))
+    shutil.copyfile(source_path,dest_path)
     return (source_path, dest_path)
 
 def install_all(keylist):
@@ -21,21 +19,19 @@ def install_all(keylist):
 USER_HOME="/home/{}".format(user)
 
 filedict = {
-"alacritty"  :("home/alacritty.yml","{}/.config/alacritty/alacritty.yml"),
+"alacritty"  :("home/alacritty/alacritty.yml","{}/.config/alacritty/alacritty.yml"),
 "tmux"       :("home/tmux.conf","{}/.tmux.conf"),
 "zshrc"      :("home/zshrc","{}/.zshrc"),
 "zlogin"     :("home/zlogin","{}/.zshrc"),
 "Xresources" :("home/Xresources","{}/.Xresources"),
-"micro"      :("home/micro.json","{}/.config/micro/settings.json"),
-"i3"         :("i3/i3config","{}/.i3/config"),
+"micro"      :("home/micro/bindings.json","{}/.config/micro/bindings.json"),
+"i3"         :("i3/i3config","{}/.config/i3/config"),
 "i3status"   :("i3/i3status.conf","{}/.i3status.conf")
 }
 
 for k in filedict.keys():
     (src,dst)   = filedict[k]
     filedict[k] = (src,dst.format(USER_HOME))
-
-print(filedict)
 
 if __name__== "__main__":
 
@@ -44,6 +40,4 @@ if __name__== "__main__":
     parser.add_argument('-m', '--mod', type=str, required=True, help=help_msg)
     args     = parser.parse_args()
 
-    print(args.mod)
-    print(filedict.contains_key(args.mod))
-    print(install("tmux"))
+    install(args.mod)
